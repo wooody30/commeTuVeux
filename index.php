a9593c5d9d93f082a310c54e145753e450c60a6e
JuOkami <justin.candela@gmail.com>
2023-09-07 16:06:15 +0200

    Mise en forme header

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
-<?php get_header(); ?>
+<!DOCTYPE html>
 
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <?php wp_head(); ?>
+</head>
+<body<?php body_class(); ?>>
+<header class="header">
+    <nav>
+        <a href="<?php echo home_url ('/');?>">
+            <img class="logo" src="<?php echo get_template_directory_uri();?>/img/logo-ns.png" alt="">
+        </a>
+        <span onclick="openNav()" class="adrouate">&#9776; </span>
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <?php wp_nav_menu(); ?>
+        </div>
+    </nav>
+    <script>
+        function openNav() {
+            document.getElementById("mySidenav").style.left = "85%";
+        }
+
+        function closeNav() {
+            document.getElementById("mySidenav").style.left = "100%";
+        }
+    </script>
+</header>
 
     <h1 class="titreprincipal">La vérité n'est pas ailleurs</h1>
 <?php //get_template_part('parts/newsletter');?>
