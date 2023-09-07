3d0dbde2150f71865d55a052b6172f5042d99e0b
Garcia-Pardo Thomas <137761880+Barsth@users.noreply.github.com>
2023-09-07 13:46:23 +0200

    Sidebar

--- header.php
+++ header.php
@@ -19,6 +19,20 @@
             <a href="<?php echo home_url ('/');?>">
                 <img class="logo" src="<?php echo get_template_directory_uri();?>/img/logo-ns.png" alt="">
             </a>
-            <?php wp_nav_menu(); ?>
+            <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+            <div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <?php wp_nav_menu(); ?>
+            </div>
         </nav>
+
+        <script>
+            function openNav() {
+                document.getElementById("mySidenav").style.width = "15%";
+            }
+
+            function closeNav() {
+                document.getElementById("mySidenav").style.width = "0";
+            }
+        </script>
 <?php wp_body_open(); ?>
