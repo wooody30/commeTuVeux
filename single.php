05301c6123b5230172a37717cf95a17301f8df86
JuOkami <justin.candela@gmail.com>
2023-09-07 13:53:00 +0200

    Liens vers les articles

--- single.php
+++ single.php
@@ -3,7 +3,6 @@
     <article class="post">
         <h1>La vérite : <?php the_title(); ?></h1>
         <div class="post_meta">
-            <?php echo get_avatar(get_the_author_meta('ID'), 40); ?>
             <p>
                 Vérité révélée aux esprits libres par <?php the_author(); ?> le <?php the_date(); ?>
                 Catégorie :  <?php the_category(); ?>
