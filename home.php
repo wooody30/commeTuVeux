1f53113ecb7b18e2f6ae7fb71f8b6d18172f70e4
JuOkami <justin.candela@gmail.com>
2023-09-07 13:49:43 +0200

    Liens vers les articles

--- home.php
+++ home.php
@@ -1,16 +1,18 @@
 <?php get_header(); ?>
 <h1>Le Mag</h1>
 <?php if(have_posts()) : while( have_posts()) : the_post(); ?>
+
+
 <article class="post">
     <?php the_category(); ?>
-    <h2><?php the_title();?></h2>
+    <a href="<?php the_permalink(); ?>"><h2><?php the_title();?></h2></a>
     <?php the_post_thumbnail(); ?>
     <p class="post_meta">
         Vérité révélée aux esprits libres par <?php the_author(); ?> le <?php the_date(); ?>
     </p>
-
     <?php the_excerpt(); ?>
-
 </article>
+
+
 <?php endwhile;endif;?>
 <?php get_footer(); ?>
