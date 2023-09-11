f36d55b544e30598b58c9d07ad6bca7dbdfe1a3e
JuOkami <justin.candela@gmail.com>
2023-09-11 11:19:56 +0200

    bouton néon du futur

--- index.php
+++ index.php
@@ -35,6 +35,6 @@
 <p>Face à la censure et à la bien-pensance des merdias, nous avons décidé de maintenir ce site d'informations alternatif
 et objectif pour rendre compte de la vraie réalité de notre monde, sans céder aux injonctions des puissants qui voudraient nous faire taire.</p>
 <p>Bienvenue et gardez les yeux ouverts.</p>
-<a href="<?php echo home_url ('/liste');?>"><h2>CLIQUEZ ICI SI VOUS N'ÊTES PAS UN MOUTON</h2></a>
+<a href="<?php echo home_url ('/liste');?>"><button class='glowing-btn'><span class='glowing-txt'>C<span class='faulty-letter'>liquez</span> ici sgdjnygrf,jnrfyhvous n'êtes pas un mouton</span></button></a>
 
 <?php get_footer(); ?>
