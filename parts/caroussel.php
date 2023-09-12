efb866745ee513cee5ddc9eee305a09f44fd5cb4
JuOkami <justin.candela@gmail.com>
2023-09-12 11:48:13 +0200

    caroussel v2

--- parts/caroussel.php
+++ parts/caroussel.php
@@ -1,3 +1,4 @@
+<a href="#grandtitre">
 <div class="caroussel">
     <img src="<?php echo get_template_directory_uri();?>/img/2.png" alt="" class="photoCaroussel">
     <img src="<?php echo get_template_directory_uri();?>/img/3.png" alt="" class="photoCaroussel">
@@ -5,3 +6,4 @@
     <img src="<?php echo get_template_directory_uri();?>/img/5.png" alt="" class="photoCaroussel">
     <img src="<?php echo get_template_directory_uri();?>/img/6.png" alt="" class="photoCaroussel">
 </div>
+</a>
